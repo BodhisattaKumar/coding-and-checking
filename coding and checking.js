e06bfd90d88408d32e1627147184d5a15770f0f8
Bodhisattakumar <bodhisattakumar@gmail.com>
2025-09-11 16:36:30 +0530

    update

---
+++ coding and checking.js
@@ -0,0 +1,79 @@
+console.log(25 + 5);   
+console.log(25 - 5);   
+console.log(25 * 5);   
+console.log(25 / 5);   
+
+console.log(123 % 10);  //find the rem
+
+
+console.log(1/0);
+console.log(-1/0);
+console.log(0/0);
+console.log(123/123);
+
+
+
+let first = "bodhisatta";
+let last = "kumar";
+console.log(first + " " + last);
+
+console.log("hi paplu da");
+
+//unary
+console.log(typeof 123);
+console.log(typeof asdf);
+console.log(typeof false);
+
+console.log(10 > 50);
+console.log(10 < 50);
+console.log(10 == 10);
+console.log(10 != 10);
+console.log(10 === 10);
+
+//compare
+console.log("apple" < "banana");
+console.log("pen" < "apple");
+
+//logical
+console.log(true && false);
+console.log(true || false);
+console.log(!false);
+
+
+let  num = 7878;
+console.log(num % 2 == 0 ? "even" : "odd");
+
+
+let a;
+let b = null;
+console.log(a);
+console.log(b);
+
+
+//type conversion
+console.log(8 * null);     
+console.log("5" - 1);
+console.log("5" + 1);
+console.log("five" * 2);    
+console.log(false == 0);   
+
+
+//=== ==
+console.log(null == undefined);  
+console.log(null == 0);          
+console.log("" == false);        
+console.log("" === false);       
+
+
+//truthly AND false 
+console.log(Boolean("Hello"));  
+console.log(Boolean(""));       
+console.log(Boolean(0));        
+console.log(Boolean(123));      
+
+//&& ||
+console.log(null || "user");   
+console.log("Karl" || "user"); 
+
+console.log(0 && "Hello");
+console.log(1 && "Hello");     

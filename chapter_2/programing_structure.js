6b0f3e34d79644359fb350d22be145db293ee095
Bodhisattakumar <bodhisattakumar@gmail.com>
2025-09-15 04:12:03 +0530

    update

---
+++ chapter_2/programing_structure.js
@@ -0,0 +1,80 @@
+
+
+
+
+
+
+
+
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+//Pick a number and check
+rl.question("Pick a number: ", (input) => {
+  const num = Number(input);
+
+  if (!isNaN(num)) {
+    console.log("Square is " + num * num);
+
+    if (num < 10) console.log("Small");
+    else if (num < 100) console.log("Medium");
+    else console.log("Large");
+  } else {
+    console.log("That’s not a number!");
+  }
+
+  //Ask name
+  rl.question("Who are you? ", (name) => {
+    if (!name) name = "Stranger";
+    console.log("Hello " + name);
+
+    //Ask weather
+    rl.question("What’s the weather? ", (weather) => {
+      switch (weather) {
+        case "rainy":
+          console.log("Bring umbrella");
+          break;
+        case "sunny":
+          console.log("Dress lightly");
+          break;
+        case "cloudy":
+          console.log("Go outside");
+          break;
+        default:
+          console.log("Unknown weather");
+      }
+
+      rl.close(); 
+    });
+  });
+});
+
+
+
+// Loops 
+console.log("While loop:");
+var number = 0;
+while (number <= 12) {
+  console.log(number);
+  number += 2;
+}
+
+console.log("For loop:");
+for (var number = 0; number <= 12; number += 2) {
+  console.log(number);
+}
+
+console.log("Break and continue:");
+for (var current = 20;; current++) {
+  if (current % 7 == 0) break;
+}
+console.log("The number is:", current);
+
+for (let i = 1; i <= 10; i++) {
+  if (i % 2 !== 0) continue;
+  console.log(i);
+}
---
+++ chapter_3(functions)/fun.js
@@ -0,0 +1,87 @@
+var Square = function(x)
+{
+    return x * x;
+
+}
+
+console.log(Square(12));
+
+
+//parameteriesed unparameterised
+var power = function(base, exponent)
+{
+    var result = 1;
+    for( var count= 0; count < exponent; count++)
+        result = result * base;
+    return result;
+
+}
+console.log(power(2,4));
+
+
+
+var makeNoise = function() {
+  console.log("Pling!");
+}
+makeNoise();
+
+
+//a fun takes a name and returs hello
+var greet = function(name){
+    return "hello, " + name;
+
+}
+console.log(greet("bodhi"))
+
+
+//global variable 
+var x = "outside ";
+
+var f1 = function() {
+  var x = "inside f1";
+}
+f1();
+console.log(x);
+
+var f2 = function() {
+  x = "inside f2"; 
+}
+
+f2();
+console.log(x);
+
+
+//mountain
+var landscape = function(){
+    var result = "";
+
+    var flat = function(size)
+    {
+        for(var count = 0; count < size; count++)
+            result += "_";
+
+
+    }
+
+    var mountain = function(size)
+
+    {
+        result += "/";
+        for(var count =0; count <size; count++)
+            result += "'";
+        result += "\\";
+    }
+
+    flat(3);
+    mountain(4);
+    flat(6);
+    mountain(1);
+    flat(1);
+
+
+
+    return result;
+
+}
+
+console.log(landscape());

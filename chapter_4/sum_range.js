448b99847169b3cfb50361ba0766e267f036517e
Bodhisattakumar <bodhisattakumar@gmail.com>
2025-09-22 10:17:31 +0530

    update

---
+++ chapter_4/sum_range.js
@@ -0,0 +1,40 @@
+// sum_range
+function range(start, end, step = 1) {
+  let arr = [];
+  
+  if (step > 0) {
+    for (let i = start; i <= end; i += step) {
+      arr.push(i);
+    }
+  } else if (step < 0) {
+    for (let i = start; i >= end; i += step) {
+      arr.push(i);
+    }
+  }
+
+  return arr;
+}
+
+
+function sum(numbers) {
+  let total = 0;
+  for (let num of numbers) {
+    total += num;
+  }
+  return total;
+}
+
+
+console.log(range(1, 10));        
+console.log(range(1, 10, 2));     
+console.log(range(5, 2, -1));     
+console.log(sum(range(1, 10)));   
+
+console.log(range(1, 10).join(", "));       
+console.log(range(1, 10, 2).join(", "));     
+console.log(range(5, 2, -1).join(", "));     
+console.log(sum(range(1, 10)));               
+
+
+
+//need some more

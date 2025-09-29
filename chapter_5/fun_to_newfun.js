fe0855000131584c952053ddd354ea9e54c4a2f6
Bodhisattakumar <bodhisattakumar@gmail.com>
2025-09-29 10:42:17 +0530

    update

--- chapter_5/fun_to_newfun.js
+++ chapter_5/fun_to_newfun.js
@@ -8,3 +8,20 @@ function greaterThan(n) {
 var greaterThan10 = greaterThan(10);
 
 console.log(greaterThan10(11)); 
+
+
+
+//Building my Own If & Loop with Functions
+function unless(test, then) {
+  if (!test) then();
+}
+
+function repeat(times, body) {
+  for (var i = 0; i < times; i++) body(i);
+}
+
+repeat(3, function(n) {
+  unless(n % 2, function() {
+    console.log(n, "is even");
+  });
+});

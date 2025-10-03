3c20d7572dc6f64a7230ac1d5738ce8b14664129
Bodhisattakumar <bodhisattakumar@gmail.com>
2025-10-03 18:53:17 +0530

    update

---
+++ chapter_7/view.js
@@ -0,0 +1,15 @@
+function View(world, vector) {
+  this.world = world;
+  this.vector = vector;
+}
+
+View.prototype.look = function(dir) {
+  var target = this.vector.plus(directions[dir]);
+  if (this.world.grid.isInside(target))
+    return charFromElement(this.world.grid.get(target));
+  else
+    return "#";
+};
+
+View.prototype.findAll = function(ch) { return []; };
+View.prototype.find = function(ch) { return null; };
